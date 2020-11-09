aaead9c23cff9e124829700391caac2c5c1b4eba
izzyuri <uriegasisrael@gmail.com>
2020-11-09 13:03:55 -0600

    done

---
+++ lib/genre.rb
@@ -0,0 +1,40 @@
+class Genre
+    extend Concerns::Findable
+    attr_accessor :name
+    attr_reader :songs
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.create(name)
+        genre = new(name)
+        genre.save
+        genre
+    end
+
+    def add_song(song)
+     song.artist = self unless song.artist
+     songs << song unless songs.include?(song)
+    end
+
+    def artists
+        songs.collect{|s| s.artist}.uniq
+    end
+
+end
